9484fe61a44cfa17a1b2b45a76de2297b4c84906
Audra Johnson <audrakjohnson@gmail.com>
2014-01-20 20:22:59 -0800

    Add basic module parsing

--- fastqc_parser.py
+++ fastqc_parser.py
@@ -2,10 +2,23 @@ import re
 import logging
 
 log = logging.getLogger(__name__)
- 
+
+PASS_RESULT="pass"
+WARN_RESULT="warn"
+FAIL_RESULT="fail"
+
+RESULTS = {
+  PASS_RESULT: PASS_RESULT,
+  WARN_RESULT: WARN_RESULT,
+  FAIL_RESULT: FAIL_RESULT
+}
+
 class FastQCParser(object):
 
     def __init__(self, filename):
+
+        self.modules = dict()
+        self.module_results = dict()
         self.input = open(filename, 'r')
         self._parse()
         self.input.close()
@@ -14,16 +27,48 @@ class FastQCParser(object):
 
         self._parse_version()
 
+        line = self.input.readline()
+        while line:
+            self._parse_module(line)
+            line = self.input.readline()
+
     def _parse_version(self):
 
         line = self.input.readline().strip()
         m = re.match('##FastQC\t(?P<version>[0-9a-z.]+)', line)
-        
+
         if m:
             self.version = m.group('version')
             log.debug("Version: %s" % self.version)
         else:
             self.version = None
             log.error("Could not find version")
+
+        return
+
+    def _parse_module(self, line):
+
+        m = re.match('>>(?P<modulename>[-a-zA-Z 0-9%]+)\t(?P<result>pass|warn|fail)', line)
+
+        if not m:
+            log.error("Could not find module name for %s" % line)
+            return
+
+        modulename = m.group('modulename')
+        result = m.group('result')
+
+        log.debug("Module: %s (%s)" % (modulename, result))
+
+        # ensure we are using the values defined as constants
+        self.module_results[modulename] = RESULTS[result]
+
+        content = []
+
+        line = self.input.readline()
+        while not line.startswith(">>END_MODULE"):
+            content.append(line)
+            line = self.input.readline()
+
+        self.modules[modulename] = {'raw_content': "".join(content)}
 
         return
--- test.py
+++ test.py
@@ -1,14 +1,40 @@
 import unittest
-import fastqc_parser
+import logging
+import sys
+
+from fastqc_parser import FastQCParser, PASS_RESULT, FAIL_RESULT, WARN_RESULT
 
 class TestBasicParsing(unittest.TestCase):
 
+    expected_results = {
+        'Per base sequence quality': PASS_RESULT,
+        'Per sequence quality scores': PASS_RESULT,
+        'Per base sequence content': WARN_RESULT,
+        'Per base GC content': WARN_RESULT,
+        'Per sequence GC content': FAIL_RESULT,
+        'Per base N content': PASS_RESULT,
+        'Sequence Length Distribution': PASS_RESULT,
+        'Basic Statistics': PASS_RESULT,
+        'Sequence Duplication Levels': PASS_RESULT,
+        'Overrepresented sequences': FAIL_RESULT,
+        'Kmer Content': FAIL_RESULT,
+    }
+
     def setUp(self):
         self.file = "examples/basic.txt"
-        self.parser = fastqc_parser.FastQCParser(self.file)
+        self.parser = FastQCParser(self.file)
 
     def test_version(self):
-        self.assertEqual(self.parser.version, '0.10.1') 
+        self.assertEqual(self.parser.version, '0.10.1')
+
+    def test_modules_present(self):
+        self.assertItemsEqual(self.parser.modules.keys(), self.expected_results.keys())
+
+    def test_module_results(self):
+        for module in self.expected_results.keys():
+            self.assertEqual(self.expected_results[module], self.parser.module_results[module])
 
 if __name__ == '__main__':
+    logging.basicConfig( stream=sys.stderr )
+    logging.getLogger( "fastqc_parser" ).setLevel( logging.DEBUG )
     unittest.main()
